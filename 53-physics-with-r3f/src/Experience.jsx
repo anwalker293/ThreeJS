86b26738015a1437c3a40571d815e90e373dcc4f
Alex Walker <alex.walker@indicio.tech>
2023-10-16 11:01:07 -0700

    feat: save physics work
    Signed-off-by: Alex Walker <alex.walker@indicio.tech>

--- 53-physics-with-r3f/src/Experience.jsx
+++ 53-physics-with-r3f/src/Experience.jsx
@@ -1,39 +1,88 @@
-import { OrbitControls } from '@react-three/drei'
+import { useGLTF, OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
-import { BallCollider, CuboidCollider, RigidBody, Physics } from "@react-three/rapier"
-import { useRef } from 'react'
+import { InstancedRigidBodies, CylinderCollider, BallCollider, CuboidCollider, RigidBody, Physics } from "@react-three/rapier"
+import { useMemo, useEffect, useState, useRef } from 'react'
 import { useFrame } from "@react-three/fiber"
 import * as THREE from 'three'
 
 export default function Experience()
 {
-    const cube = useRef()
-    const twister = useRef()
-    // -- addForce --
-    // - Used to apply a force that lasts for quite a long time
-    // (e.g. wind)
-    // -- applyImpulse --
-    // - Used to apply a force for a very short period of time
-    // (e.g. projectile)
-    const cubeJump = () => {
-      cube.current.applyImpulse({x:0, y:5, z:0})
-      cube.current.applyTorqueImpulse({
-        x: Math.random() - 0.5,
-        y: Math.random() - 0.5,
-        z: Math.random() - 0.5
-      })
-    }
+    // const [ hitSound ] = useState(() => {
+    //   new Audio("./hit.mp3")
+    // })
+    // const cube = useRef()
+    // // const cubes = useRef()
+    // const cubesCount = 3
+    // const twister = useRef()
+    // // -- addForce --
+    // // - Used to apply a force that lasts for quite a long time
+    // // (e.g. wind)
+    // // -- applyImpulse --
+    // // - Used to apply a force for a very short period of time
+    // // (e.g. projectile)
+    // const cubeJump = () => {
+    //   cube.current.applyImpulse({x:0, y:5, z:0})
+    //   cube.current.applyTorqueImpulse({
+    //     x: Math.random() - 0.5,
+    //     y: Math.random() - 0.5,
+    //     z: Math.random() - 0.5
+    //   })
+    // }
+
+    // -- 4 different events --
+    // onCollisionEnter - when the RigidBody hit something
+    // onCollisionExit - when the RigidBody separates from the
+    //                    object it just hit
+    // onSleep - when the RigidBody starts sleeping
+    // onWake - when the RigidBody stops sleeping
     
-    useFrame((state) => {
-      const time = state.clock.getElapsedTime()
-      const eulerRotation = new THREE.Euler(0, time, 0)
-      const quaternionRotation = new THREE.Quaternion()
-      quaternionRotation.setFromEuler(eulerRotation)
-      twister.current.setNextKinematicRotation(quaternionRotation) 
-    })
+    // useFrame((state) => {
+    //   const time = state.clock.getElapsedTime()
+    //   const eulerRotation = new THREE.Euler(0, time*3, 0)
+    //   const quaternionRotation = new THREE.Quaternion()
+    //   quaternionRotation.setFromEuler(eulerRotation)
+    //   twister.current.setNextKinematicRotation(quaternionRotation) 
+    //   const angle = time * 0.5;
+    //   const x = Math.cos(angle);
+    //   const z = Math.sin(angle);
+    //   twister.current.setNextKinematicTranslation({x: x, y: -0.8, z:z})
+    // })
+      
+    {/* const collisionEnter = () => {
+      hitSound.currentTime = 0
+      hitSound.volume = Math.random()
+      hitSound.play()
+    } */}
 
-    return <>
+    const hamburger = useGLTF("./hamburger.glb")
 
+        // Matrix4 combo of postition, rotation, and scale
+        {/* useEffect(() => {
+          for (let i = 0; i < cubesCount; i++) {
+            const matrix = new THREE.Matrix4()
+            matrix.compose(
+              new THREE.Vector3(i * 2, 0, 0),
+              new THREE.Quaternion(),
+              new THREE.Vector3(1, 1, 1)
+            )
+            cubes.current.setMatrixAt(i, matrix)
+          }
+        }, []) */}
+
+        {/* const instances = useMemo(() => {
+            const instances = []
+
+            for(let i = 0; i < cubesCount; i++)
+            {
+                instances.push({
+                    key: 'instance_' + i,
+                    position: [ i * 2, 0, 0 ],
+                    rotation: [ 0, 0, 0 ],
+                })
+            }
+
+            return instances
+        }, []) */}
         <Perf position="top-left" />
 
         <OrbitControls makeDefault />
@@ -66,6 +115,10 @@ export default function Experience()
             restitution={ 0 }
             friction={ 0.7 }
             colliders={ false }
+            onCollisionEnter={ collisionEnter }
+            // onCollisionExit={() => (console.log("exit"))}
+            // onSleep={() => (console.log("sleep"))}
+            // onWake={() => {console.log("wake")}}
           >
             <mesh castShadow onClick={ cubeJump }>
                 <boxGeometry />
@@ -92,7 +145,29 @@ export default function Experience()
               <meshStandardMaterial color="red" />
             </mesh>
           </RigidBody> 
-        </Physics>
+
+          <RigidBody colliders={false} position={[0, 4, 0]}>
+            <primitive object={hamburger.scene} scale={0.25} />
+            <CylinderCollider args={[0.5, 1.25]} />
+          </RigidBody>
+
+
+          <RigidBody type="fixed">
+            <CuboidCollider args={ [ 5, 2, 0.5 ] } position={ [ 0, 1, 5.5 ] } />
+            <CuboidCollider args={ [ 5, 2, 0.5 ] } position={ [ 0, 1, - 5.5 ] } />
+            <CuboidCollider args={ [ 0.5, 2, 5 ] } position={ [ 5.5, 1, 0 ] } />
+            <CuboidCollider args={ [ 0.5, 2, 5 ] } position={ [ - 5.5, 1, 0 ] } />
+          </RigidBody>        
+          
+          <InstancedRigidBodies instances={instances}>
+            <instancedMesh castShadow receiveShadow args={ [ null, null, cubesCount ] }>
+              <boxGeometry />
+              <meshStandardMaterial color="tomato" />
+            </instancedMesh>
+          </InstancedRigidBodies> 
+    </Physics>
+
+        
 
     </>
 }
